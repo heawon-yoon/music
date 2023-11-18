b5e2211a31319816fd7462a842335ebcf794f6ce
hunkunai <heawon122@gmail.com>
2023-11-18 18:58:12 +0800

    init

--- app.py
+++ app.py
@@ -34,10 +34,12 @@ from custom.enhance_tg import enhance_tg
 from custom.build_dataset import build_dataset
 from custom.add_ph_num import add_ph_num
 from custom.estimate_midi import estimate_midi
-from custom.convert_ds import csv2ds
 from custom.combine_ds import combine_ds
 from custom.path_util import get_full_path
 from custom.audio import merge_wav
+import librosa
+import copy
+import random
 
 
 
@@ -69,8 +71,10 @@ rstd = None
 def gen_lyc(audio):
     
      global rstd
-     rst = uvr("HP5-主旋律人声vocals+其他instrumentals","","opt1"
-     ,audio,"opt1",10,"wav")
+     global lyc_data
+     tpath = random.randint(1, 100)
+     rst = uvr("HP5-主旋律人声vocals+其他instrumentals","","opt2/"+str(tpath)
+     ,audio,"opt2/"+str(tpath),10,"wav")
      print("rst",rst)
      rstd = rst
      print("rstd",rstd)
@@ -80,21 +84,42 @@ def gen_lyc(audio):
      asr = ASRExecutor()
      rst_data = []
      dir_tmp_path = None
-     for slice in slice_data:
+     cp_slice_data = copy.deepcopy(slice_data)
+     tmp_i = 0
+     for i,slice in enumerate(cp_slice_data):
          try:
              #语音转文字
              print(slice["wav_path"])
              tmp_audio = slice["wav_path"]
+                
+             audio, sr = librosa.load(slice["wav_path"])
+             duration = librosa.get_duration(audio, sr)
+             print(tmp_audio)
+             print(duration)
              result = asr(audio_file=tmp_audio,force_yes=True)
              print(result)
-             lyc = {}
-             lyc["length"]=len(result)
-             lyc["text"]=result
-             lyc_data.append(lyc)
-             rst_data.append(result)
-            
-         except:
-             print("%s->Suc." % slice["wav_path"])
+             if result is None or result=='':
+                os.remove(tmp_audio)
+                del slice_data[i-tmp_i]
+                tmp_i +=1
+             else:
+                
+                lyc = {}
+                lyc["length"]=len(result)
+                lyc["text"]=result
+                lyc_data.append(lyc)
+                rst_data.append(result)
+
+                #文字转音速.lab文件
+                dir_path = os.path.dirname(tmp_audio)
+                name, extension = os.path.splitext(tmp_audio)
+                lab_file_path = os.path.join(dir_path, name + '.lab')
+                pinyin2phs=pinyin2ph_func(result)
+                print("pinyins :"+" ".join(pinyin2phs))
+                with open(lab_file_path, "w") as file:
+                    file.write(" ".join(pinyin2phs))
+         except Exception as e:
+             print("%s->Suc." % e)
      return "\n".join(rst_data)
 def run_acoustic(text_input,text_speaker,audio):
      #人声分离
@@ -113,17 +138,7 @@ def run_acoustic(text_input,text_speaker,audio):
              #语音转文字
              print(slice["wav_path"])
              tmp_audio = slice["wav_path"]
-             result = text_input.split("\n")[i]
-             print(result)
-             #文字转音速.lab文件
              dir_path = os.path.dirname(tmp_audio)
-             name, extension = os.path.splitext(tmp_audio)
-             lab_file_path = os.path.join(dir_path, name + '.lab')
-             pinyin2phs=pinyin2ph_func(result)
-             print("pinyins :"+" ".join(pinyin2phs))
-             with open(lab_file_path, "w") as file:
-                 file.write(" ".join(pinyin2phs))
-             
              dir_tmp_path = dir_path
                 
             
@@ -152,7 +167,7 @@ def run_acoustic(text_input,text_speaker,audio):
      os.system("python custom/convert_ds.py csv2ds %s %s " % (dir_tmp_path+"/tmp/transcriptions.csv", dir_tmp_path+"/tmp"))
     
      #生成推理文件
-     ds_file = combine_ds(dir_tmp_path+"/tmp",slice_data)
+     ds_file = combine_ds(dir_tmp_path+"/tmp",slice_data,text_input)
       
      #开始推理   
      os.system("python scripts/infer.py acoustic %s --exp my_experiment --spk opencpop --out %s" % (ds_file,os.path.dirname(ds_file)))
@@ -165,29 +180,52 @@ def flip_image(x):
     return np.fliplr(x)
 with gr.Blocks() as demo:
     #用markdown语法编辑输出一段话
-    gr.Markdown("Flip text or image files using this demo.")
+    gr.Markdown("混鲲AI大模型")
     # 设置tab选项卡
-    with gr.Tab("cover"):
+    with gr.Tab("AI魔改翻唱"):
         #Blocks特有组件，设置所有子组件按垂直排列
         #垂直排列是默认情况，不加也没关系
         with gr.Column():
-            audio = gr.File(file_count="multiple",label="上传音频", info="需上传wav格式")
+            audio = gr.File(file_count="multiple",label="上传音频", info="需上传wav格式",height="100")
             lyr_button = gr.Button("生成歌词")
             text_input = gr.Textbox(label="歌词魔改", info="一定要按原来歌词字数一样!")
             text_speaker =  gr.Dropdown(
-            ["beyong", "ldh", "wf"], label="原声", info="Will add more animals later!"
+            ["牛夫人", "Beyong", "妖姬"], label="AI原声", info="Will add more animals later!"
             )
             text_button = gr.Button("推理AI音频")
             vacal_audio = gr.Audio(label="AI音频", info="合成音频最终显示在这里")
-    with gr.Tab("video"):
+    with gr.Tab("AI歌词"):
+        #Blocks特有组件，设置所有子组件按水平排列
+        with gr.Row():
+            image_input = gr.Image()
+            image_output = gr.Image()
+        image_button = gr.Button("Flip")
+    with gr.Tab("AI音频+视频"):
+        #Blocks特有组件，设置所有子组件按水平排列
+        with gr.Row():
+            image_input = gr.Image()
+            image_output = gr.Image()
+        image_button = gr.Button("Flip")
+    with gr.Tab("AI声音转换"):
         #Blocks特有组件，设置所有子组件按水平排列
         with gr.Row():
             image_input = gr.Image()
             image_output = gr.Image()
         image_button = gr.Button("Flip")
     #设置折叠内容
-    with gr.Accordion("Open for More!"):
-        gr.Markdown("Look at me...")
+    with gr.Accordion("推理流程!"):
+        gr.Markdown(
+                 """
+                1.音频文件uvr人声分离
+                2.人声降噪,美化,增强
+                3.autoSliece人声按片段智能AI切片
+                4.ASR人声自动识别转歌词
+                5.生成歌词后可以手动修改歌词
+                6.MFA歌词音频自动强制对齐
+                7.数据预处理
+                8.开始推理
+                9.人声跟伴奏重新合成生成新音频
+                 """)
     text_button.click(run_acoustic, inputs=[text_input,text_speaker,audio], outputs=[text_input,vacal_audio])
     lyr_button.click(gen_lyc, inputs=[audio], outputs=[text_input])
-demo.launch(server_name='127.0.0.1',server_port=7861)
+demo.launch(server_name='172.21.182.86',server_port=7861)
